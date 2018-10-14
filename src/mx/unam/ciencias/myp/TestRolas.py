ec82c577aec42c879634d971f2fb2f7cba571738
Teresa Becerril <terebece1508@ciencias.unam.mx>
2018-10-13 21:25:34 -0500

    Agregando esqueleto para las pruebas unitarias de Rolas

---
+++ src/mx/unam/ciencias/myp/TestRolas.py
@@ -0,0 +1,35 @@
+import unittest
+import Rolas
+import Miner
+
+class TestPerson(unittest.TestCase):
+
+    def testGetPath(self):
+
+    def testGetTitle(self):
+
+    def testGetPerformer(self):
+
+    def testGetAlbum(self):
+
+    def testGetYear(self):
+
+    def testGetTrack(self):
+
+    def testGetGenre(self):
+
+    def testGetPicture(self):
+
+    def testSetTitle(self):
+
+    def testSetPerformer(self):
+
+    def testSetAlbum(self):
+
+    def testSetYear(self):
+
+    def testSetTrack(self):
+
+    def testSetGenre(self):
+
+    def testSetPicture(self):
